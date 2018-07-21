fae7bf1cab5d949cdbff8ae91c2e6b0186351d79
jeff <jeffersondarcy@users.noreply.github.com>
2018-07-21 20:39:38 +0200

    destination form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
 import './App.css';
+import SelectDestination from './components/SelectDestination'
 
 import logo from './logo.svg';
 
+type Screen = 'SelectDestination' | 'SelectItinerary';
+
 class App extends React.Component {
+  public state: {
+    activeScreen: Screen;
+    time?: number;
+  };
+  public props: {};
+  constructor(props: {}) {
+    super(props);
+    this.state = { activeScreen: 'SelectDestination' };
+    this.setDate = this.setDate.bind(this);
+  }
+
+  public setDate(timestamp: number) {
+    this.setState({
+      activeScreen: 'SelectItinerary',
+      time: timestamp,
+    });
+  }
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          Get started.
         </p>
+        <SelectDestination setDate={this.setDate}/>
       </div>
     );
   }
