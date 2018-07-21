aa64cf227b50bfb66f9a273dbf36ca4f0daaff77
Sergii Katrych <sergii.katrych@gmail.com>
2018-07-21 18:11:00 +0200

    Smart Contracts: removing template contract from the project

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-var BiddingLib = artifacts.require("./Bidding.sol");
 var TravelTargets = artifacts.require("./TravelTargets.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(BiddingLib)
-    .then(() => {
-    deployer.deploy(TravelTargets, TravelTargets.address);
-  });
+  deployer.deploy(TravelTargets);
 };
